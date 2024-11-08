c00424b93ac2e3a59bfc2174b74adf8aec8a714b
DTron <darrenstorey87@gmail.com>
2024-11-08 23:03:31 +0000

    Add dynamic path handling for WSL and Windows compatibility.

--- importtorch.py
+++
@@ -1,4 +0,0 @@
-import torch
-print(torch.__version__)          # Should print version with +cu118
-print(torch.cuda.is_available())  # Should return True
-print(torch.cuda.get_device_name(0))  # Should print "NVIDIA GeForce GTX 1080 Ti"
---
+++ test.py
@@ -0,0 +1,2 @@
+import bitsandbytes as bnb
+print("BitsAndBytes Loaded Successfully")
